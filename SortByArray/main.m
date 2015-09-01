341d5cf52b5e173ea8a3caa1910207eefc6874fc
RexMa88 <rex.smith98@gmail.com>
2015-09-01 10:29:23 +0800

    projectOne

--- SortByArray/main.m
+++ SortByArray/main.m
@@ -14,8 +14,14 @@ int main(int argc, const char * argv[]) {
         // insert code here...
         NSArray *array = @[[NSNumber numberWithInt:12],[NSNumber numberWithInt:23],[NSNumber numberWithInt:2],[NSNumber numberWithInt:15],[NSNumber numberWithInt:16],[NSNumber numberWithInt:8],[NSNumber numberWithInt:45],[NSNumber numberWithInt:32]];
         NSArray *sortArray = [array sortedArrayUsingSelector:@selector(compare:)];
+        //another method sort
+        NSMutableArray *arrayOne = [[NSMutableArray alloc] initWithObjects:@"24",@"12",@"48",@"35",@"7",@"2",@"3",@"65", nil];
+        
+        NSSortDescriptor *sortDescriptor = [NSSortDescriptor sortDescriptorWithKey:@"self" ascending:YES];
+        NSArray *sortArrayOne = [arrayOne sortedArrayUsingDescriptors:[NSArray arrayWithObjects:sortDescriptor, nil]];
         
         NSLog(@"The sortArray is %@",sortArray);
+        NSLog(@"Another sortArray is %@",sortArrayOne);
         
         NSLog(@"Hello, World!");
     }
